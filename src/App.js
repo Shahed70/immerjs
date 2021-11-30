1863d1c007f8e37efd9565387718ac7837415572
Shahed <shahedbd123@gmail.com>
2021-11-30 16:01:18 +0600

    immer practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import produce from 'immer';
+import { useCallback, useState } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id: "React",
+      title: "Learn React",
+      done: true
+    },
+    {
+      id: "Immer",
+      title: "Try Immer",
+      done: false
+    }
+  ]);
+
+  const handleToggle = useCallback((id) => {
+    console.log('Rendered');
+    setTodos(
+      produce((draft) => {
+        const todo = draft.find( (todo) => todo.id === id);
+        todo.done = !todo.done;
+      })
+    )
+
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    console.log('Rendered');
+    setTodos(
+      produce((draft) => {
+        draft.push({
+          id:"Angular",
+          title: "todo_" + Math.random(),
+          done:true
+        })
+      })
+    )
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Practice immer js</h1>
+          {
+            todos?.map( (todo, index) => (
+              <ul key={index}>
+                <li> {todo.title} </li>
+                <li> {todo?.done ? "Task is done":"Task is not done"}</li>
+              </ul>
+            ))
+          }
+          <button onClick={()=> handleToggle("Angular")}>Toggle</button>
+          <button onClick={handleAdd}>AddData</button>
     </div>
   );
 }
